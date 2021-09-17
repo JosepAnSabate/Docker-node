97c14a24c44f0b0eda1f47e5380075cd4d68347e
josep.andreu@e-campus.uab.cat <josep.andreu@e-campus.uab.cat>
2021-09-17 19:00:50 +0200

    dbconnected

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express")
-
+const mongoose = require('mongoose');
 const app = express()
+
+
+mongoose
+    .connect("mongodb://josep.andreu%40e-campus.uab.cat:celdoni@192.168.144.2:27017/?authSource=admin")
+    .then(() => console.log("succesfully connected to DB"))
+    .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
     res.send("<h2>Hi There!!!</h2>");
